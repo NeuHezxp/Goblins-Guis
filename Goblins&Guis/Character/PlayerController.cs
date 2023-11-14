f945dde8b0bed2c0ecb5b9b893cb5372a0da2901
NeuHezxp <mpatino@student.neumont.edu>
2023-11-14 08:12:39 -0700

    added comments code still works

--- Goblins&Guis/Character/PlayerController.cs
+++ Goblins&Guis/Character/PlayerController.cs
@@ -22,29 +22,27 @@ namespace Goblins_Guis
         }
         public void BindProperties()
         {
+            
             CharacterCreationForm form = (CharacterCreationForm)CharacterCreationForm;
             //form variables
-            //var nameTextBox = (TextBox)CharacterCreationForm.Controls["nameTextBox"];
-            //var textBox1 = (TextBox)CharacterCreationForm.Controls["textBox1"];
-            //var classComboBox = (ComboBox)CharacterCreationForm.Controls["classComboBox"];
             var nameTextBox = CharacterCreationForm.Controls["nameTextBox"] as TextBox;
             var textBox1 = CharacterCreationForm.Controls["textBox1"] as TextBox;
             var classComboBox = CharacterCreationForm.Controls["classComboBox"] as ComboBox;
             // Bind the NumericUpDown controls
-            form.StrengthNumericUpDown.DataBindings.Add("Value", player, "STR", false, DataSourceUpdateMode.OnPropertyChanged);
-            form.DexterityNumericUpDown.DataBindings.Add("Value", player, "DEX", false, DataSourceUpdateMode.OnPropertyChanged);
-            form.ConstitutionNumericUpDown.DataBindings.Add("Value", player, "CON", false, DataSourceUpdateMode.OnPropertyChanged);
-            form.IntelligenceNumericUpDown.DataBindings.Add("Value", player, "INT", false, DataSourceUpdateMode.OnPropertyChanged);
-            form.WisdomNumericUpDown.DataBindings.Add("Value", player, "WIS", false, DataSourceUpdateMode.OnPropertyChanged);
-            form.CharismaNumericUpDown.DataBindings.Add("Value", player, "CHA", false, DataSourceUpdateMode.OnPropertyChanged);
+            form.StrengthNumericUpDown.DataBindings.Add     ("Value", player, "STR", false, DataSourceUpdateMode.OnPropertyChanged);
+            form.DexterityNumericUpDown.DataBindings.Add    ("Value", player, "DEX", false, DataSourceUpdateMode.OnPropertyChanged);
+            form.ConstitutionNumericUpDown.DataBindings.Add ("Value", player, "CON", false, DataSourceUpdateMode.OnPropertyChanged);
+            form.IntelligenceNumericUpDown.DataBindings.Add ("Value", player, "INT", false, DataSourceUpdateMode.OnPropertyChanged);
+            form.WisdomNumericUpDown.DataBindings.Add       ("Value", player, "WIS", false, DataSourceUpdateMode.OnPropertyChanged);
+            form.CharismaNumericUpDown.DataBindings.Add     ("Value", player, "CHA", false, DataSourceUpdateMode.OnPropertyChanged);
 
 
             //name and textbox combo
-            nameTextBox.DataBindings.Add("Text", player, "Name");
-            textBox1.DataBindings.Add("Text", player, "Gender");
+            nameTextBox.DataBindings.Add    ("Text", player, "Name");
+            textBox1.DataBindings.Add       ("Text", player, "Gender");
             classComboBox.DataSource = Enum.GetValues(typeof(Character.CharacterClass));
-            classComboBox.DataBindings.Add("SelectedItem", player, "Class");
-
+            classComboBox.DataBindings.Add  ("SelectedItem", player, "Class");
+            
         }
     }
 }
--- Goblins&Guis/CharacterCreationForm.cs
+++ Goblins&Guis/CharacterCreationForm.cs
@@ -29,16 +29,18 @@ namespace Goblins_Guis
 
 
             // Bind player properties to controls
-            //nameTextBox.DataBindings.Add("Text", playerCharacter, "Name");
-            //textBox1.DataBindings.Add("Text", playerCharacter, "Gender");
-            //classComboBox.DataSource = Enum.GetValues(typeof(Character.CharacterClass));
-            //classComboBox.DataBindings.Add("SelectedItem", playerCharacter, "Class");
-            //numericUpDownSTR.DataBindings.Add("Value", playerCharacter, "STR", false, DataSourceUpdateMode.OnPropertyChanged);
-            //numericUpDownDEX.DataBindings.Add("Value", playerCharacter, "DEX", false, DataSourceUpdateMode.OnPropertyChanged);
-            //numericUpDownCON.DataBindings.Add("Value", playerCharacter, "CON", false, DataSourceUpdateMode.OnPropertyChanged);
-            //numericUpDownINT.DataBindings.Add("Value", playerCharacter, "INT", false, DataSourceUpdateMode.OnPropertyChanged);
-            //numericUpDownWIS.DataBindings.Add("Value", playerCharacter, "WIS", false, DataSourceUpdateMode.OnPropertyChanged);
-            //numericUpDownCHA.DataBindings.Add("Value", playerCharacter, "CHA", false, DataSourceUpdateMode.OnPropertyChanged);
+            /*
+            nameTextBox.DataBindings.Add("Text", playerCharacter, "Name");
+            textBox1.DataBindings.Add("Text", playerCharacter, "Gender");
+            classComboBox.DataSource = Enum.GetValues(typeof(Character.CharacterClass));
+            classComboBox.DataBindings.Add("SelectedItem", playerCharacter, "Class");
+            numericUpDownSTR.DataBindings.Add("Value", playerCharacter, "STR", false, DataSourceUpdateMode.OnPropertyChanged);
+            numericUpDownDEX.DataBindings.Add("Value", playerCharacter, "DEX", false, DataSourceUpdateMode.OnPropertyChanged);
+            numericUpDownCON.DataBindings.Add("Value", playerCharacter, "CON", false, DataSourceUpdateMode.OnPropertyChanged);
+            numericUpDownINT.DataBindings.Add("Value", playerCharacter, "INT", false, DataSourceUpdateMode.OnPropertyChanged);
+            numericUpDownWIS.DataBindings.Add("Value", playerCharacter, "WIS", false, DataSourceUpdateMode.OnPropertyChanged);
+            numericUpDownCHA.DataBindings.Add("Value", playerCharacter, "CHA", false, DataSourceUpdateMode.OnPropertyChanged);
+            */
         }
 
         private void StartButton_click(object sender, EventArgs e)
